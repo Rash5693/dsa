ee3ca2257643b0cd653561143aae3b5acc61d0f6
Rachana <109274692+Rash5693@users.noreply.github.com>
2025-08-04 20:44:52 +0530

    Time: 29 ms (94.59%), Space: 72.7 MB (40.4%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -9,32 +9,32 @@ var threeSum = function(nums)
 
     for (let i = 0; i < nums.length - 2; i++) 
     {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; 
+        if (i > 0 && nums[i] == nums[i - 1]) continue; 
 
-        let left = i + 1;
-        let right = nums.length - 1;
+        let j = i + 1;
+        let k = nums.length - 1;
 
-        while (left < right) 
+        while (j < k) 
         {
-            const sum = nums[i] + nums[left] + nums[right];
+            const sum = nums[i] + nums[j] + nums[k];
 
             if (sum === 0) 
             {
-                result.push([nums[i], nums[left], nums[right]]);
+                result.push([nums[i], nums[j], nums[k]]);
        
-                while (left < right && nums[left] === nums[left + 1]) left++;
-                while (left < right && nums[right] === nums[right - 1]) right--;
+                while (j < k && nums[j] == nums[j + 1]) j++;
+                while (j < k && nums[k] == nums[k - 1]) k--;
 
-                left++;
-                right--;
+                j++;
+                k--;
             } 
             else if (sum < 0) 
             {
-                left++; 
+                j++; 
             } 
             else 
             {
-                right--; 
+                k--; 
             }
         }
     }

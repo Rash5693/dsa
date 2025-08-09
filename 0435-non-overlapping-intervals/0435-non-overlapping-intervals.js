e2c6ab4ad2989c4466f540ec822c5b5d7a5dcfca
Rachana <109274692+Rash5693@users.noreply.github.com>
2025-08-09 17:27:37 +0530

    Time: 89 ms (29.03%), Space: 85.5 MB (85.21%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) 
+{
+    if(intervals.length == 0) return 0;
+
+    intervals.sort((a,b) => a[1] - b[1]);
+
+    let count = 0;
+     let previousEnd = intervals[0][1];
+
+     for(let i = 1; i < intervals.length; i++)
+     {
+        if(intervals[i][0] < previousEnd)
+        {
+            count++;
+        }
+        else
+        {
+            previousEnd = intervals[i][1];
+        }
+     }
+     return count;
+}
